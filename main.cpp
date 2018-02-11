a352171f55aa628882fbaf106055f7bc86ff6274
Nekhil Nagia <nekhilnagia16@gmail.com>
2018-02-10 19:13:26 -0500

    Added basic 2 player functionality. Few improvements remaining to final PvsP release

--- main.cpp
+++ main.cpp
@@ -13,34 +13,90 @@ using namespace std;
 vector<vector<int>> mainGrid;
 
 //Store the dimensions of the grid
-int gridDimesnions = 3;
+int gridDimensions = 3;
+
+//Variable to hold whos turn it is
+//Player 1 gets Xs and player 2 gets Ys
+int whosTurn = 1;
+
+//AI toggle
+bool isAIPlaying = false;
+
+//Bool to hold if endgame reached
+bool isWin = false;
 
 //Initialize the mainGrid with zeroes when given dimensions by the user
 void initializeGrid(){
 //    cout << "Enter nxn dimensions of grid: ";
-//    cin >> gridDimesnions;
+//    cin >> gridDimensions;
 
-    vector<int> emptyVec(gridDimesnions, 0);
-    for (int i = 0; i < gridDimesnions; ++i) {
+    vector<int> emptyVec(gridDimensions, 0);
+    for (int i = 0; i < gridDimensions; ++i) {
         mainGrid.push_back(emptyVec);
     }
 }
 
 //Subroutine for displaying the grid visually in the console
 void printGrid(){
-    for (int i = 0; i < gridDimesnions; ++i) {
-        for (int j = 0; j < gridDimesnions; ++j) {
-            cout << mainGrid[j][i] << " | ";
+    string symbol;
+    int symbolVal = 0;
+    for (int i = 0; i < gridDimensions; ++i) {
+        for (int j = 0; j < gridDimensions; ++j) {
+
+            symbolVal = mainGrid[j][i];
+
+            if(symbolVal == X) symbol = "X";
+            else if(symbolVal == O) symbol = "O";
+            else if(symbolVal == 0) symbol = " ";
+
+            cout << symbol << " | ";
         }
         cout << endl;
-        for(int x = 0; x < (gridDimesnions*gridDimesnions)+2; x++) cout << "-";
+        for(int x = 0; x < (4*gridDimensions)-1; x++) cout << "-";
         cout << endl;
     }
+
+    cout << endl << endl;
 }
 
+//subroutine for updating grid when a player makes a turn
+void playerTurn(int TurnSymbol, int xLoc, int yLoc){
+    mainGrid[xLoc][yLoc] = TurnSymbol;
+    printGrid();
+}
+
+void simulateGame(){
+    while(!isWin) {
+        int xLoc = 0, yLoc = 0;
+        if (!isAIPlaying) {
+            cout << "Enter the x and y coordinates: ";
+            cin >> xLoc >> yLoc;
+        }
+
+        if (whosTurn == 1) {
+            playerTurn(X, xLoc, yLoc);
+            whosTurn = 2;
+        } else if (whosTurn == 2) {
+            playerTurn(O, xLoc, yLoc);
+            whosTurn = 1;
+        }
+    }
+
+
+}
+
+/*
+ * TO DO:
+ * Add endgame checking logic
+ * no overwriting allowed functionality
+ * More dynamic simulate game (by allowing user(s) to select who goes first)
+ * AI Implementation
+ * */
 int main() {
     initializeGrid();
-    printGrid();
+
+    simulateGame();
+
     return 0;
 }
 
